0e94151abde06e67f8f808622cb6869d1aaf54e0
Ezequiel <eperez@itba.edu.ar>
2022-11-27 19:43:16 -0300

    chore: improve styles

---
+++ components/Editor/index.tsx
@@ -0,0 +1,75 @@
+import { ChangeEvent, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/230#issuecomment-1133934152
+import docco from 'react-syntax-highlighter/dist/cjs/styles/hljs/docco'
+import Textarea from 'react-textarea-autosize'
+import s from "./Editor.module.scss"
+
+const INITIAL_CONTENT = [
+  "# Probando el titulo de la nota",
+  "Un parrafo tranqui con `inline code`",
+  "- Un bullet",
+  "- Y otro mas",
+  "Un poquito de codigo para cerrar",
+  '```js\n console.log("Hello world")\n db.find({ id: 1 }) \n ```',
+  "![](https://1000marcas.net/wp-content/uploads/2021/06/Redis-Logo.png)",
+  "Mas texto por aca"
+]
+
+export default function Editor() {
+  const [content, setContent] = useState(INITIAL_CONTENT)
+
+  // TODO: "enter" deberia crear un nuevo bloque, que en markdown es con doble enter, por lo que habria que sumar un \n manualmente
+  // podriamos hacer que "shift+enter" inserte dos espacios y un \n, que es un linebreak normal en markdown
+  const handleBlockInput = (e: ChangeEvent<HTMLTextAreaElement>, index: number) => {
+    setContent(oldContent => {
+      const newContent = [...oldContent]
+      newContent[index] = e.target.value
+      return newContent
+    })
+  }
+
+  return (
+    <div className={s.container}>
+      {/* FIXME: using index as key won't work ok if we reorder / delete / add blocks in the middle of the array */}
+      <div className={s.blocks}>
+        {content.map((block, index) => (
+          <div key={index} className={s.block}>
+            <button>+</button>
+            <Textarea value={block} onChange={(e) => handleBlockInput(e, index)} />
+            <span className={s.lineNum}>{index}</span>
+          </div>
+        ))}
+      </div>
+
+      <div className={s.preview}>
+        {content.map((block, index) => (
+          <ReactMarkdown
+            children={block}
+            key={index}
+            components={{
+              code: ({ node, inline, className, children, ...props }) => {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, '')}
+                    style={docco}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  />
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                )
+              }
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  )
+
+}
