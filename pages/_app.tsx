78fad852abd0344b43bd61dd2c0d94f95a8ee39a
Ezequiel <eperez@itba.edu.ar>
2022-11-27 18:18:19 -0300

    feat: basic editor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <>
+    <Head>
+      <title>Remarque</title>
+      <meta name="description" content="The notes app by excellence." />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
