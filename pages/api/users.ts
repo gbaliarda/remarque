4eaa9ed2ca7ab8ab0f91a3786aad532b26edc1f7
gbaliarda <gbaliarda@itba.edu.ar>
2022-12-01 18:45:31 -0300

    feat: get user using email query param endpoint implementation

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -13,21 +13,48 @@ import User from '../../models/user'
  *     responses:
  *       200:
  *         description: User created successfully
+ *   get:
+ *     description: Retrieves user information using email
+ *     tags:
+ *       - Users
+ *     responses:
+ *       200:
+ *         description: A single user
+ *     parameters: 
+ *       - name: email
+ *         in: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Email of user to retrieve
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).end(`Method ${req.method} not allowed`)
-  }
-
   await connectMongo().catch(e => res.status(500).json({ e }))
 
-  try {
-    const { email, password } = req.body
-    const hashedPassword = await bcrypt.hash(password, 12)
-    await User.create({ email, password: hashedPassword })
-    res.status(201).json({ msg: "Successfully registered user" })
-  } catch (e) {
-    console.log(e)
-    res.status(400).json({ msg: e })
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { email } = req.query
+        await User.findOne({ email }).then(user => res.status(200).json({ _id: user._id, email: user.email, notes: user.notes}))
+      } catch (e) {
+        console.log(e)
+        res.status(400).json({ msg: e })
+      }
+      break
+    case 'POST':
+      try {
+        const { email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 12)
+        await User.create({ email, password: hashedPassword })
+        res.status(201).json({ msg: "Successfully registered user" })
+      } catch (e) {
+        console.log(e)
+        res.status(400).json({ msg: e })
+      }
+      break
+    default:
+      return res.status(405).end(`Method ${req.method} not allowed`)
   }
+
+
 }
