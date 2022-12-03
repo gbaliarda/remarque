ce9cd32d16dfdc8aa1d62da8296376bb082aae6a
Ezequiel <eperez@itba.edu.ar>
2022-12-03 02:03:59 -0300

    feat: index page api connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
 import Link from 'next/link'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { signIn, signOut } from "next-auth/react"
+import { GetServerSideProps } from 'next'
+import { unstable_getServerSession } from "next-auth/next"
+// @ts-ignore
+import { authOptions } from 'pages/api/auth/[...nextauth]'
 import s from '../styles/Home.module.scss'
+import { createNote } from '../services/notes'
+import { useRouter } from 'next/router'
+import { apiFetcher } from '../services/setup';
+import { Note, User } from '../services/types'
 
-const NOTES = [
-  { id: "123-abc", title: "TPE Bases de Datos 2"},
-  { id: "424-fde", title: "Apuntes Metodos Numericos"},
-  { id: "984-ftw", title: "Apuntes Economia"},
-]
+interface Props {
+  user: User | null
+  notes: Note[]
+}
 
-export default function Home() {
-  const { data: session } = useSession()
+export default function Home({ user, notes }: Props) {
+  const router = useRouter()
+
+  const handleCreate = async () => {
+    try {
+      const note = await createNote()
+      router.push(`/notes/${note._id}`)
+    } catch(e) {
+      console.log(e)
+    }
+  }
   
-  if (!session) return (
+  if (!user) return (
     <div className={s.login}>
       <p>Not signed in</p>
       <button className={s.button} onClick={() => signIn()}>Sign in</button>
@@ -21,20 +37,43 @@ export default function Home() {
   return (
     <>
       <nav className={s.nav}>
-        <p>Signed in as {session.user!!.email}</p>
+        <p>Signed in as {user.email}</p>
         <button className={s.button} onClick={() => signOut()}>Sign out</button>
       </nav>
       <main className={s.main}>
-        
         <h1>My Notes</h1>
         <div className={s.notes}>
-          {NOTES.map(note => (
-            <Link key={note.id} passHref href={`/notes/${note.id}`}>
+          {notes.map(note => (
+            <Link key={note._id} passHref href={`/notes/${note._id}`}>
               <a className={s.note}>{note.title}</a>
             </Link>
           ))}
         </div>
+        <button onClick={handleCreate} className={s.create}>+ New</button>
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+
+  if (!session) {
+    return {
+      props: { user: null, notes: [] }
+    }
+  }
+
+  // If an error is thrown inside getServerSideProps, it will show the pages/500.js file. Check out the documentation for 500 page to learn more on how to create it. During development this file will not be used and the dev overlay will be shown instead.
+  const user = await apiFetcher<User>(`/api/users?email=${session.user!!.email}`)
+  // https://github.com/nextauthjs/next-auth/issues/4238#issuecomment-1251750371
+  const notes = await apiFetcher<Note[]>(`/api/users/${user._id}/notes`, {
+    // @ts-ignore
+    headers: {
+      Cookie: context.req.headers.cookie
+    }
+  })
+  return {
+    props: { user, notes }
+  }
 }
